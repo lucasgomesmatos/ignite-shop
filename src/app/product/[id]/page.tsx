3d768781633d8fef83ae5734e1c2568be79fe203
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-04-04 22:53:29 -0300

    pages nextjs

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+export default function ProductId() {
+  const pathName = usePathname().slice(-1);
+
+  return <div>{pathName}</div>;
+}
