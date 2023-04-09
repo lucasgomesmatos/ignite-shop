c4fb6910e8e434368071b4a25efbf6e3b42312a5
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-04-08 23:49:53 -0300

    carrossel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,51 @@
+'use client';
 import { HomeContainer, Product } from '@/styles/pages/home';
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
 
 import camiseta1 from '../assets/camisetas1.png';
 import camiseta2 from '../assets/camisetas2.png';
+import camiseta3 from '../assets/camisetas3.png';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      {/* <Product>
-          <Image src={camiseta3} alt="" width={520} height={480} />
-          <footer>
-            <strong>Camiseta x</strong>
-            <span>R$ 79,90</span>
-          </footer>
-        </Product> */}
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
     </HomeContainer>
   );
 }
