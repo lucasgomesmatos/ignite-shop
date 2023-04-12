ffe519a14882fdf4b28405dc50340a48334f4a83
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-04-12 08:21:36 -0300

    fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
 
+import { useEffect, useState } from 'react';
 import camiseta1 from '../assets/camisetas1.png';
 import camiseta2 from '../assets/camisetas2.png';
 import camiseta3 from '../assets/camisetas3.png';
@@ -16,8 +17,17 @@ export default function Home() {
     },
   });
 
+  const [list, setList] = useState<number[]>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setList([1, 2, 3]);
+    }, 2000);
+  }, []);
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} />
         <footer>
